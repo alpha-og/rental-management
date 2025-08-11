a4a2786b6050c049456e40f6776614907a8eb5c1
Athul Anoop <athulanoop2020@gmail.com>
2025-08-12 01:34:24 +0530

    fix: typo in white-listed server address

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
     // Enable CORS
     app.enableCors({
         // temporarily hardcoded origins
-        origin: ["http://localhost:3000", "http:35.222.216.48"],
+        origin: ["http://localhost:3000", "http://35.222.216.48"],
         credentials: true,
     });
 
