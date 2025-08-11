c14fc7f41b2faae3b02417a4a466e78c50fba221
Athul Anoop <athulanoop2020@gmail.com>
2025-08-12 01:21:11 +0530

    chore: cleanup unused imports and arguments

--- apps/server/src/associations/associations.module.ts
+++ apps/server/src/associations/associations.module.ts
@@ -1,12 +1,10 @@
-import { Inject, Module, OnModuleInit } from "@nestjs/common";
+import { Module, OnModuleInit } from "@nestjs/common";
 import { applyAssociations } from "./associations.service";
-import { Sequelize } from "sequelize-typescript";
 
 @Module({})
 export class AssociationsModule implements OnModuleInit {
-    constructor(@Inject(Sequelize) private readonly sequelize: Sequelize) {}
+    constructor() {}
     onModuleInit() {
-        console.log("Applying associations...", this.sequelize);
         applyAssociations();
     }
 }
