40f7828db48d9ee76d58bd107f559e909be3fbd5
Athul Anoop <athulanoop2020@gmail.com>
2025-08-11 14:54:59 +0530

    update: swagger api documentation generation configuration

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import cookieParser from "cookie-parser";
 import { ValidationPipe } from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 const PORT = process.env.PORT ?? 4000;
 
@@ -14,6 +15,17 @@ async function bootstrap() {
             transform: true,
         }),
     );
+
+    // Swagger API Documentation
+    const config = new DocumentBuilder()
+        .setTitle("Rental Management API")
+        .setDescription("API for rental management")
+        .setVersion("0.1")
+        .addTag("rental")
+        .build();
+    const documentFactory = () => SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup("api", app, documentFactory);
+
     await app.listen(PORT);
 }
 
